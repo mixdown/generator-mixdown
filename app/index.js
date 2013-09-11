543ca5e48002a2e11e80e90ce9d6ad9d55367f4d
Eric Capps <eric@vast.com>
2013-09-11 17:06:24 -0500

    First pass at a yeoman generator for mixdown

---
+++ app/index.js
@@ -0,0 +1,29 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var MixdownGenerator = module.exports = function MixdownGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(MixdownGenerator, yeoman.generators.Base);
+
+MixdownGenerator.prototype.howdy = function () {
+  console.log(this.yeoman);
+};
+
+MixdownGenerator.prototype.app = function app() {
+  var callback = this.async();
+
+  this.remote('mixdown', 'boilerplate', function (err, remote) {
+    remote.directory('./', './');
+    callback();
+  });
+};
