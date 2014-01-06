2b9e9d1560fc0f5887b1b6e63ee31fa8b8ced1db
Tommy Messbauer <tommy@vast.com>
2014-01-06 17:26:33 -0600

    clearing old stuff

--- app/templates/local-router/router.js
+++
@@ -1,5 +0,0 @@
-var Router = require('../scaffold/router/router.js');
-
-Router.prototype.home = require('./controllers/home.js');
-
-module.exports = Router;
--- app/templates/scaffold/router/controllers/manifest.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(httpContext) {
-  var app = httpContext.app;
-  var headers = app.config.plugins.static.options.headers;
-
-  app.plugins.json(app.plugins.router.routes, httpContext.response);
-};
